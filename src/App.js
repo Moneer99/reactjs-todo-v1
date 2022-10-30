47a2f0f90004a366144091ab394e7499d9f35e53
moneer99 <moneerbinoon9@gmail.com>
2022-10-30 23:17:10 +0300

    add todos comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
 import Header from "./component/layout/Header";
+import Todos from "./component/Todos";
 import Footer from "./component/layout/Footer";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: Math.floor(Math.random() * 100) + 1,
+        title: "Meeting with boss",
+        completed: false,
+      },
+      {
+        id: Math.floor(Math.random() * 100) + 1,
+        title: "Take out the trash",
+        completed: false,
+      },
+      {
+        id: Math.floor(Math.random() * 100) + 1,
+        title: "Pick up kids from school",
+        completed: false,
+      },
+    ],
+  };
+
   render() {
     return (
       <>
         <Header />
+        <Todos />
         <Footer />
       </>
     );
