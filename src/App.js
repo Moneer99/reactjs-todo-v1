abd6f05f893c3fef19cb28e28f5037993860d2d4
moneer99 <moneerbinoon9@gmail.com>
2022-10-30 22:17:00 +0300

    create header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Header from "./component/layout/Header";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <>
-        <h1>welcome to first change</h1>
+      <Header/>
       </>
     );
   }
